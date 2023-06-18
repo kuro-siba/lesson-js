4ad5e349c9aa7a125a03c328b24ebd11d4248ce2
kuro-siba <85010116+kuro-siba@users.noreply.github.com>
2023-06-18 11:47:15 +0900

    alert

--- 04-work2/js/script.js
+++ 04-work2/js/script.js
@@ -3,4 +3,10 @@ const count = document.querySelector('#count');
 
 text.addEventListener('keyup',() => {
   count.textContent = text.value.length;
+
+  if (text.value.length > 100) {
+    count.classList.add('alert');
+  } else {
+    count.classList.remove('alert')
+  }
 });
