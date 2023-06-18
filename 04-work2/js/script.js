16a0fab29b93d7e1159de16e86d4c1a57ebe6d08
kuro-siba <85010116+kuro-siba@users.noreply.github.com>
2023-06-18 10:42:30 +0900

    5

--- 04-work2/js/script.js
+++ 04-work2/js/script.js
@@ -2,5 +2,6 @@ const text = document.querySelector('#text');
 const count = document.querySelector('#count');
 
 text.addEventListener('keyup', () => {
+  console.log("1")
   count.textContent = text.Value.length;
 });
